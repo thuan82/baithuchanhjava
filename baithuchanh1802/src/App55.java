ea2f80e820aa5230fee1477d1c14dd7fe0add338
thuan82 <thuanbrave8@gmail.com>
2022-02-18 19:06:54 +0700

    Create_Project_baithuchanh1802

---
+++ baithuchanh1802/src/App55.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class App55{
+
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("Cac so chia het cho 5 la: ");
+
+        for (int i = 0; i <= 20; i++){
+
+            continue;
+
+        }
+
+        System.out.println();
+
+    }
+
+}
