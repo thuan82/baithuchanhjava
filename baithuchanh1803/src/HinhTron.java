d9ce697e917a37f33651e82ddc8c7d232bfcd156
thuan82 <thuanbrave8@gmail.com>
2022-03-18 11:07:05 +0700

    Create_Project_baithuchanh1803

--- baithuchanh1803/src/HinhTron.java
+++ baithuchanh1803/src/HinhTron.java
@@ -1,5 +1,41 @@
-public class App {
+import java.util.Scanner;
+public class HinhTron {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HinhTron ht = new HinhTron();
+        ht.NhapBanKinh();
+        ht.TinhChuVi();
+        ht.inChuVi();
+        ht.TinhDienTinh();
+        ht.inDienTich();
+    }
+
+    final float PI = 3.14f;
+    float r;
+    float cv;
+    float dt;
+    void NhapBanKinh()
+    {
+        System.out.print("Nhap vao ban kinh hinh tron: ");
+        Scanner sc = new Scanner(System.in);
+        r = sc.nextFloat();
+        sc.close();
+    }
+
+    void TinhChuVi()
+    {
+        cv = 2 * PI * r;
+    }
+
+    void TinhDienTinh()
+    {
+        dt = PI * r * r;
+    }
+    void inChuVi()
+    {
+        System.out.print("\nChu vi hinh tron: " + cv);
+    }
+    void inDienTich()
+    {
+        System.out.print("\nDien tich hinh tron: " + dt);
     }
 }
