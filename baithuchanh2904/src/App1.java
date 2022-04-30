c579b643bdef69a3b79df8c77dc5e04df026294a
thuan82 <thuanbrave8@gmail.com>
2022-04-30 19:51:34 +0700

    Create_Project_baithuchanh2904

---
+++ baithuchanh2904/src/App1.java
@@ -0,0 +1,10 @@
+import Shape.Triangle;
+
+public class App1 {
+    public static void main(String[] args) throws Exception{
+        Triangle tri = new Triangle();
+        tri.nhaptamgiac();
+        tri.kiemTraLoaiTamGiac();
+
+    }
+}
---
+++ baithuchanh2904/src/App2.java
@@ -0,0 +1,10 @@
+import Shape.Tugiac;
+
+public class App2 {
+    public static void main(String[] args) throws Exception {
+        Tugiac b = new Tugiac();
+        b.nhapTuGiac();
+        b.kiemTraTuGiac();
+        b.kiemTraLoaiTuGiac();
+    }
+}
